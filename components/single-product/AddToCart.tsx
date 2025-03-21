d8457c8e9bec04263115447571dea2df32169d4b
Mihailghrgh <mihailghrgh@gmail.com>
2025-03-21 17:23:12 +0200

    Demo

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -1,6 +1,8 @@
 import { Button } from "../ui/button";
 
 function AddToCart({ productId }: { productId: string }) {
+  console.log(productId);
+
   return (
     <Button className="capitalize mt-8 rounded-none" size="lg">
       Add To Cart
