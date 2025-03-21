006cfefad042092bb20d0fe783822cb1466916e8
Mihailghrgh <mihailghrgh@gmail.com>
2025-03-21 17:52:12 +0200

    TEST

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -6,13 +6,17 @@ import NavSearch from "./NavSearch";
 import CartButton from "./CartButton";
 import DarkMode from "./DarkMode";
 import LinksDropdown from "./LinksDropdown";
+import { Suspense } from "react";
 
 function Navbar() {
   return (
     <nav className="border-b-4">
       <Container className="pb-5 lg:pb-0 flex flex-col justify-between items-center sm:flex-row flex-wrap gap-6">
         <Logo />
-        <NavSearch />
+        <Suspense>
+          <NavSearch />
+        </Suspense>
+
         <div className="flex items-center justify-center gap-24 md:gap-8">
           <CartButton />
           <DarkMode />
